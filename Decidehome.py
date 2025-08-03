63d2d69a12cbcef72009cd1d4e54308f84ffc774
Sem <sambennett4243@gmail.com>
2025-08-03 15:35:11 +0100

    Tidied up a bunch of the home network hardcoding

--- Decidehome.py
+++ Decidehome.py
@@ -2,13 +2,14 @@ import hassapi as hass
 
 class Decidehome(hass.Hass):
     def initialize(self):
+        self.home_network = "VM2072540"
         self.listen_state(self.set_presence, "sensor.sm_a145r_wi_fi_connection")
         self.log("Listening for arrivals/departures..")
 
     def set_presence(self, entity, attribute, old, new, kwargs):
         #Used because when I'm out if I disconnect and reconnect to random wifis it triggers
-        if old != "VM2072540" and new != "VM2072540":
+        if old != self.home_network and new != self.home_network:
             return
-        state = "on" if new == "VM2072540" else "off"
+        state = "on" if new == self.home_network else "off"
         self.log(f"Setting at_home to {state}")
         self.call_service("input_boolean/turn_" + state, entity_id="input_boolean.at_home")
